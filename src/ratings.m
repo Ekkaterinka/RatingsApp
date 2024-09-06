2a850cccc642c912d7337e9f3f3f382bf2000731
Ekaterina <e.belova@microline.ru>
2024-09-06 13:38:41 +0300

    fix arguments func appStoreReview

--- src/ratings.m
+++ src/ratings.m
@@ -9,11 +9,12 @@
     NSNumber* sufficientNumberVisits = [command.arguments objectAtIndex:1];
     
     if (@available(iOS 10.3, *)) {
+        
         if (numberVisits >= sufficientNumberVisits) {
             [SKStoreReviewController requestReview];
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"AppStoreReview start"];
         } else {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"AppStoreReview error"];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"The condition did not pass"];
         }
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 
--- www/ratings.js
+++ www/ratings.js
@@ -1,5 +1,8 @@
+cordova.define("cordova-plugin-app-store-ratings-ios.appStoreReview", function(require, exports, module) {
 var exec = require('cordova/exec');
 
-exports.appStoreReview= function (arg0, success, error) {
-    exec(success, error, 'ratings', 'appStoreReview', [arg0]);
+exports.appStoreReview= function (numberVisits,sufficientNumberVisits, success, error) {
+    exec(success, error, 'ratings', 'appStoreReview', [numberVisits,sufficientNumberVisits]);
 };
+
+});
