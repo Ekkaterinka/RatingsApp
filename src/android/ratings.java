f116123ea54609195ef328e0805852976cc1678c
Ekaterina <e.belova@microline.ru>
2024-09-09 10:35:49 +0300

    edit java

--- src/android/ratings.java
+++ src/android/ratings.java
@@ -1,34 +1,35 @@
-package com.coloz.ratings;
+package com.microline.zont.ratings;
 
+import com.google.android.gms.tasks.*;
+import com.google.android.play.core.review.ReviewException;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
-import com.google.android.play.core.review.ReviewRequest;
+import com.google.android.play.core.review.model.ReviewErrorCode;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 
 public class ratings extends CordovaPlugin {
-  
+
   protected void appStoreReview(int numberVisits, CallbackContext callbackContext) {
-      if (numberVisits >= 10) {
-        ReviewManager manager = ReviewManagerFactory.create(this);
-          Task<ReviewInfo> request = manager.requestReviewFlow();
-          request.addOnCompleteListener(task -> {
+      if (numberVisits >10) {
+        ReviewManager manager = ReviewManagerFactory.create(cordova.getContext());
+        Task<ReviewInfo> request = manager.requestReviewFlow();
+        request.addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
-                
+
                 ReviewInfo reviewInfo = task.getResult();
             } else {
-             
+
                 @ReviewErrorCode int reviewErrorCode = ((ReviewException) task.getException()).getErrorCode();
             }
           });
-          callbackContext.success(numberVisits);
+        callbackContext.success(numberVisits);
       } else {
           callbackContext.error("Expected one non-empty string argument.");
       }
