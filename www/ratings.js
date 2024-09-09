8fdff9901a28ec86a1f54220ed6f19fe636f2bef
Ekaterina <e.belova@microline.ru>
2024-09-09 11:00:11 +0300

    delete import cordova

--- www/ratings.js
+++ www/ratings.js
@@ -1,8 +1,5 @@
-cordova.define("cordova-plugin-app-ratings-in-store.appStoreReview", function(require, exports, module) {
 var exec = require('cordova/exec');
 
 exports.appStoreReview= function (numberVisits,sufficientNumberVisits, success, error) {
     exec(success, error, 'ratings', 'appStoreReview', [numberVisits,sufficientNumberVisits]);
 };
-
-});
